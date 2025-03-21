13d12bc50508f655a965beef86b371d5ed140ec9
DeboraSchaan <debora.schaan@gmail.com>
2025-03-21 17:31:57 -0300

    create script.js and sketch envelope logic

---
+++ public/script.js
@@ -0,0 +1,14 @@
+// array holding objects to represent each envelope might be a useful approach. 
+// Each envelope will be represented by a numerical ID and hold information regarding its budget and title.
+
+class Envelope {
+    constructor(id, category, budget) {
+        this.id = id;
+        this.category = category;
+        this.budget = budget;
+    }
+
+    // set limit
+    // set expenses
+    // get bugdet
+}
