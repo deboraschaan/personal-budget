b393b4d005e31ebd2d59284b8a1c263ae939d160
DeboraSchaan <debora.schaan@gmail.com>
2025-03-21 18:01:35 -0300

    create mock data

--- data.js
+++ data.js
@@ -1,5 +1,21 @@
-const envelopes = [];
-let totalBudget;
+const envelopes = [
+    {
+        id: 1,
+        category: 'Bills',
+        budget: 1000
+    },
+    {
+        id: 2,
+        category: 'Groceries',
+        budget: 150
+    },
+    {
+        id: 3,
+        category: 'Gas',
+        budget: 100
+    }
+];
+let totalBudget = 2000;
 
 module.exports = {
     envelopes,
