b0a98784fe55d3936f10cc86bb02abab5b570fca
DeboraSchaan <debora.schaan@gmail.com>
2025-03-31 16:16:07 -0300

    Add cors middleware and set up a proxy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const errorHandler = require('errorhandler');
+const cors = require('cors');
 const envelopesRouter = require('../routes/envelopes-router.js');
 
 const app = express();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(errorHandler());
+app.use(cors({ origin: "http://localhost/5173" }));
 
 // Routes
 app.use('/api/v1/envelopes', envelopesRouter);
